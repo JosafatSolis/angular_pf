0c5b9cb23467035e19c9bd7877269e73ac68c30b
Josafat Solis <jsolis@macroaplicaciones.com>
2022-05-20 18:22:09 -0500

    Crear alumnos.actions

--- pf_solis/src/app/alumnos/alumnos.module.ts
+++ pf_solis/src/app/alumnos/alumnos.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AlumnosRoutingModule } from './alumnos-routing.module';
-import { StoreModule } from '@ngrx/store';
-import * as fromAlumnos from './reducers';
-
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AlumnosRoutingModule,
-    StoreModule.forFeature(fromAlumnos.alumnosFeatureKey, fromAlumnos.reducers, { metaReducers: fromAlumnos.metaReducers })
+    AlumnosRoutingModule
   ]
 })
 export class AlumnosModule { }
