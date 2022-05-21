73e8d8200170f3e91201d743425d9112147960a5
Josafat Solis <jsolis@macroaplicaciones.com>
2022-05-20 19:06:09 -0500

    Agregar el store alumnos.

--- pf_solis/src/app/alumnos/alumnos.module.ts
+++ pf_solis/src/app/alumnos/alumnos.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AlumnosRoutingModule } from './alumnos-routing.module';
+import { StoreModule } from '@ngrx/store';
+import * as fromAlumnos from './reducers';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AlumnosRoutingModule
+    AlumnosRoutingModule,
+    StoreModule.forFeature(fromAlumnos.alumnosFeatureKey, fromAlumnos.reducers, { metaReducers: fromAlumnos.metaReducers })
   ]
 })
 export class AlumnosModule { }
